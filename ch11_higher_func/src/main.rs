e612998f7c1c6c63cc26043f419906eee270e0d0
张建华(30596) <2675731662@qq.com>
2024-08-17 13:17:14 +0800

    feat: add ch11_hight_func

--- ch11_higher_func/src/main.rs
+++ ch11_higher_func/src/main.rs
@@ -1,3 +1,29 @@
+fn mul_twice(f: fn(i32) -> i32, x: i32) -> i32 {
+    // f(f(x))
+    f(x)
+} 
+
 fn main() {
-    println!("Hello, world!");
+    fn add_one(x: i32) -> i32 {
+        x + 1
+    }
+    println!("{}", mul_twice(add_one, 5));
+
+    // 数学计算
+    let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let res1: Vec<_> = numbers.iter().map(|&x| x + x).collect();
+    println!("{:?}", res1);
+
+    // filter
+    // let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let res2: Vec<_> = res1.iter().filter(|&x| x % 2 == 0).collect();
+    println!("{:?}", res2);
+
+    // fold
+    // let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let sum = res2.iter().fold(0, |acc, &x| acc + x );
+    println!("sum: {:?}", sum);
+
+
 }
